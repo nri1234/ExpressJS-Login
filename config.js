53b5ba1aac7e4fefb726150aa479ec8173b8236f
Norbert Mielczarek <nri1234.nm@gmail.com>
2019-08-23 14:55:26 +0200

    Add content

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'GOOGLE_CLIENT_ID': '1014090199898-22542thnhrrnbo07sdmsppbd24qb4stg.apps.googleusercontent.com',
-    'GOOGLE_CLIENT_SECRET': 'eIte1ZTXP3wHFRicsY6damra',
+    'GOOGLE_CLIENT_ID': '1032844402036-jqlef6u9ob7t77hro83i8t11ctnkv8ig.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'v3Q1TJ5UBpy4ImRgN9BW8ldK',
     'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
 };
--- server.js
+++ server.js
@@ -1,24 +1,68 @@
-var express = require('express');
-bodyParser = require('body-parser');
+var express = require("express");
+var passport = require("passport");
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+var config = require("./config");
+bodyParser = require("body-parser");
 var app = express();
+var googleProfile = {};
 app.use(bodyParser.json());
 
-app.use(express.static('css'));
-app.use(express.static('assest'));
-
-app.set('view engine', 'pug');
-app.set('views','./views');
-
-app.get('/', function(req, res){
-    res.render('index');
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
 });
 
-app.get('/auth/google', function (req, res) {
-    res.render('google');
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: config.GOOGLE_CLIENT_ID,
+            clientSecret: config.GOOGLE_CLIENT_SECRET,
+            callbackURL: config.CALLBACK_URL
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            googleProfile = {
+                id: profile.id,
+                displayName: profile.displayName
+            };
+            cb(null, profile);
+        }
+    )
+);
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use(express.static("css"));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", function(req, res) {
+    res.render("index", { user: req.user });
 });
+
+app.get("/logged", function(req, res) {
+    res.render("logged", { user: googleProfile });
+});
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"]
+    })
+);
+app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", {
+        successRedirect: "/logged",
+        failureRedirect: "/"
+    })
+);
 
 app.listen(3000);
 
-app.use(function (req, res, next) {
-    res.status(404).send('Sorry, we could not find what you want!')
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry, we could not find what you want!");
 });
